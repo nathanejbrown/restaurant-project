dfe65623bc323e441293e6e2a43a1ebe0b952388
gvickstrom <gvickstrom@gmail.com>
2016-09-14 15:31:36 -0600

    whatever

--- src/server/db/seeds/users.js
+++ src/server/db/seeds/users.js
@@ -10,7 +10,7 @@ exports.seed = function(knex, Promise) {
         knex('users').insert({first_name: 'George', last_name: 'Vickstrom', email: 'george@george.com', password: 'george1', permissions: 'admin'}),
         knex('users').insert({first_name: 'Ryan', last_name: 'Harings', email: 'ryan@ryan.com', password: 'ryan1', permissions: 'admin'}),
         knex('users').insert({first_name: 'Turd', last_name: 'Ferguson', email: 'turd@turd.com', password: 'turd1', permissions: 'guest'}),
-        knex('users').insert({first_name: 'Harry', last_name: 'Potter', email: 'harry@harry.com', password: 'harry1', permissions: 'visitor'}),
+        knex('users').insert({first_name: 'Harry', last_name: 'Potter', email: 'harry@harry.com', password: 'harry1', permissions: 'visitor'})
       ]);
     });
 };
---
+++ src/server/routes/validations.js
@@ -0,0 +1,11 @@
+const knex = require('../db/knex');
+
+function verify (req, res, next) {
+  //setup a container to store erros
+  const errors = [];
+  //grab values from req.body
+  const firstName = req.body.preferredname;
+  const lastName = req.body.lastname;
+  const email = req.body.email;
+  const password = req.body.password;
+}
