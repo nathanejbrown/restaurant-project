a577e55c645bc3537abdca71f2c723b756b8b5b2
Ryan Harings <ryanharings@hotmail.com>
2016-09-14 14:19:31 -0600

    index and restaurants routes

--- src/server/config/route-config.js
+++ src/server/config/route-config.js
@@ -6,9 +6,11 @@
 
     // *** routes *** //
     const routes = require('../routes/index');
+    const restaurantRoutes = require('../routes/restaurants');
 
     // *** register routes *** //
     app.use('/', routes);
+    app.use('/restaurants', restaurantRoutes);
 
   };
 
