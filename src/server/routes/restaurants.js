ab4710468396ff53cd072b226aa9546225675d26
Heathcliff <dennis.nathan@gmail.com>
2016-09-15 10:23:35 -0600

    Created post route to add new restaurant

--- src/server/routes/restaurants.js
+++ src/server/routes/restaurants.js
@@ -27,4 +27,25 @@ router.get('/:id', function (req, res, next) {
     });
 });
 
+router.post('/', function (req, res, next) {
+  let newRestaurant = {
+    rating: req.body.rating,
+    name: req.body.name,
+    type: req.body.type,
+    pic_url: req.body.pic_url,
+    description: req.body.description
+  };
+  knex('restaurants')
+    .insert(newRestaurant)
+    .then(function() {
+      let returnObject = {};
+      returnObject.message = `${newRestaurant.name} was successfully added`;
+      res.render('archive', returnObject);
+    }).catch(err => {
+      let returnObject = {};
+      returnObject.message = err.message || `Sorry, ${newRestaurant.name} was not added.`;
+      res.render('error', returnObject);
+    });
+});
+
 module.exports = router;
