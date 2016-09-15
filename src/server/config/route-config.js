e2aa78fa96e03759342cf4f0969eea12b5fa203c
gvickstrom <gvickstrom@gmail.com>
2016-09-15 09:43:31 -0600

    added signin page

--- src/server/config/route-config.js
+++ src/server/config/route-config.js
@@ -7,11 +7,13 @@
     // *** routes *** //
     const routes = require('../routes/index');
     const signupRoutes = require('../routes/signup');
+    const signinRoutes = require('../routes/signin');
     const restaurantRoutes = require('../routes/restaurants');
 
     // *** register routes *** //
     app.use('/', routes);
     app.use('/signup', signupRoutes);
+    app.use('/signin', signinRoutes);
     app.use('/restaurants', restaurantRoutes);
   };
 
