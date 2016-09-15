bbab9527f8fbc40bc56ee230126029ac7c11d780
Ryan Harings <ryanharings@hotmail.com>
2016-09-15 15:07:33 -0600

    login page and functionality

--- src/server/config/route-config.js
+++ src/server/config/route-config.js
@@ -7,14 +7,12 @@
     // *** routes *** //
     const routes = require('../routes/index');
     const signupRoutes = require('../routes/signup');
-    const signinRoutes = require('../routes/signin');
     const restaurantRoutes = require('../routes/restaurants');
     const loginRoutes = require('../routes/login');
 
     // *** register routes *** //
     app.use('/', routes);
     app.use('/signup', signupRoutes);
-    app.use('/signin', signinRoutes);
     app.use('/restaurants', restaurantRoutes);
     app.use('/login', loginRoutes);
   };
--- src/server/routes/login.js
+++ src/server/routes/login.js
@@ -3,10 +3,45 @@ const router = require('express').Router();
 // const indexController = require('../controllers/index');
 const knex = require('../db/knex');
 
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10);
+const hash = bcrypt.hashSync('B4c0/\/', salt);
+
 router.get('/', function (req, res, next) {
   const renderObject = {};
   renderObject.title = 'Login Page';
   res.render('login', renderObject);
+});
+
+router.post('/', function (req, res, next) {
+  const loginEmail = req.body.loginEmail;
+  const loginPass = req.body.loginPassword;
+
+  knex('users')
+  .select('*')
+  .then(users => {
+    let [user] = users.filter(user => user.email === loginEmail);
+
+    let msg;
+    var result;
+    if (!user || !loginPass) {
+      msg = {message: 'Email or password is the wrongest!'};
+      result = JSON.stringify(msg);
+      res.render('login', {title: 'Login Page', msg: result});
+    } else {
+      // if (bcrypt.compareSync())
+      if (loginPass === user.password) {
+        res.redirect(302, '/restaurants');
+      } else {
+        msg = {message: 'Email or password is the wrongest!'};
+        result = JSON.stringify(msg);
+        res.render('login', {title: 'Login Page', msg: result});
+      }
+    }
+  })
+  .catch((err) => {
+    throw err;
+  });
 });
 
 module.exports = router;
