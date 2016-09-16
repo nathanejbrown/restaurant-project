2a6a7e028fe2b23a763cff9764642774ffb28947
gvickstrom <gvickstrom@gmail.com>
2016-09-16 09:52:50 -0600

    added functionality to signup page that rejects existing usernames

--- src/server/routes/validations.js
+++ src/server/routes/validations.js
@@ -10,33 +10,30 @@ function verify (req, res, next) {
   const password = req.body.password;
   //check if the fields are blank
   if (firstName === '') {
-    errors.push('Please Input First Name!');
+    errors.push('Please Input First Name! ');
   }
   if (lastName === '') {
-    errors.push('Please Input Last Name!');
+    errors.push('Please Input Last Name! ');
   }
   if (email === '') {
-    errors.push('Please Input an Email!');
+    errors.push('Please Input an Email! ');
   }
-  if (password === '') {
-    errors.push('Please Input a Password');
+  if (password === '' || password.length < 8) {
+    errors.push('Please Input a Password at Least 8 characters long! ');
   }
-  //is password long enough??
-  if (password.length < 8) {
-    errors.push('Password must be at least 8 characters long!');
-  }
+
   //does the username already exist??
   isUnique('email', email, (err, response) => {
     if (err) {
       return next(err);
     }
-    if (response) {
+    if (!response) {
       errors.push('Login credentials already exist!');
     }
     if (errors.length) {
       const renderObject = {};
       renderObject.errors = errors;
-      return res.render('signup', renderObject);
+      res.render('signup', renderObject);
     } else {
       return next();
     }
@@ -45,14 +42,15 @@ function verify (req, res, next) {
 
 //some helper function
 function isUnique (column, value, callback) {
+  // const mult = { error: 'already exists'};
   knex('users')
   .select()
   .where(column, value)
   .then((results) => {
     if (results.length) {
-      callback(null, true);
-    } else {
       callback(null, false);
+    } else {
+      callback(null, true);
     }
   })
   .catch((err) => {
